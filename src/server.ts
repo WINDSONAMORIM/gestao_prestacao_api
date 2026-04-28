d849d9c6b82a147592af731533494d91c88b94b4
WINDSON AMORIM MARANA <windson.amorim@gmail.com>
2026-04-28 15:57:57 -0300

    test: deploy

--- src/server.ts
+++ src/server.ts
@@ -47,8 +47,24 @@ app.register(import("./features/analytics/analytics.routes.js"))
 
 await errorHandler(app);
 
-app.listen({ port: Number(process.env.PORT) || 8080 }).then(() => {
-  console.log(
-    "🚀 Server rodando em http://localhost:" + (process.env.PORT || 8080),
-  );
-});
+const start = async () => {
+  try {
+    const port = process.env.PORT ? Number(process.env.PORT) : 3000;
+    await app.listen({
+      port,
+      host: '0.0.0.0'
+    });
+    console.log(`Server listening on http://0.0.0.0:${port}`);
+  } catch (err) {
+    app.log.error(err);
+    process.exit(1);
+  }
+};
+
+start();
+
+// app.listen({ port: Number(process.env.PORT) || 8080 }).then(() => {
+//   console.log(
+//     "🚀 Server rodando em http://localhost:" + (process.env.PORT || 8080),
+//   );
+// });
