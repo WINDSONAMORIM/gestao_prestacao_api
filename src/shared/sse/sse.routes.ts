eb564ea8e16c9f9b7af7a87ebf860ce68e81866c
WINDSON AMORIM MARANA <windson.amorim@gmail.com>
2026-06-17 18:26:37 -0300

    refactor: corrigi rota SSE

--- src/shared/sse/sse.routes.ts
+++ src/shared/sse/sse.routes.ts
@@ -1,16 +1,22 @@
 import { FastifyInstance } from "fastify";
 import { clients } from "./sse.types.js";
 
-export default async function orcadoRoutes(app: FastifyInstance) {
+export default async function sseRoutes(app: FastifyInstance) {
 app.get("/downloadProcess/events", async (request, reply) => {
   const clientId = crypto.randomUUID();
 
-  reply.raw.writeHead(200, {
-    "Content-Type": "text/event-stream",
-    "Cache-Control": "no-cache",
-    Connection: "keep-alive",
-    "Access-Control-Allow-Origin": "http://localhost:3000",
-  });
+  reply.header("Content-Type", "text/event-stream");
+  reply.header("Cache-Control", "no-cache");
+  reply.header("Connection", "keep-alive");
+
+  reply.raw.flushHeaders();
+
+  reply.raw.write(
+      `data: ${JSON.stringify({
+        connected: true,
+        clientId,
+      })}\n\n`
+    );
 
   clients.set(clientId, {
     id: clientId,
@@ -19,6 +25,7 @@ app.get("/downloadProcess/events", async (request, reply) => {
 
   request.raw.on("close", () => {
     clients.delete(clientId);
+    console.log(`Cliente ${clientId} desconectado`);
   });
 });
 }
