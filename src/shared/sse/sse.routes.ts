52875929da07af05c475beca35d1529612ce46fb
WINDSON AMORIM MARANA <windson.amorim@gmail.com>
2026-06-26 18:16:50 -0300

    refactor: SSE

--- src/shared/sse/sse.routes.ts
+++ src/shared/sse/sse.routes.ts
@@ -1,7 +1,7 @@
 import { FastifyInstance } from "fastify";
 import { clients } from "./sse.types.js";
 
-export default async function sseRoutes(app: FastifyInstance) {
+export default async function sseRoutes(app: FastifyInstance ) {
   app.options("/downloadProcess/events", (req, reply) => {
     reply
       .header("Access-Control-Allow-Origin", req.headers.origin || "*")
@@ -12,14 +12,19 @@ export default async function sseRoutes(app: FastifyInstance) {
   });
 
   app.get("/downloadProcess/events", (request, reply) => {
-    const clientId = crypto.randomUUID();
+    // Adicionar os cabeçalhos CORS aqui também
+    reply
+      .header("Access-Control-Allow-Origin", request.headers.origin || "*")
+      .header("Access-Control-Allow-Methods", "GET, OPTIONS")
+      .header("Access-Control-Allow-Headers", "Content-Type, Authorization")
+      .header("Access-Control-Allow-Credentials", "true");
 
+    const clientId = crypto.randomUUID();
     reply.raw.writeHead(200, {
       "Content-Type": "text/event-stream",
       "Cache-Control": "no-cache",
       Connection: "keep-alive",
     });
-
     reply.raw.flushHeaders();
     reply.hijack();
 
@@ -30,12 +35,11 @@ export default async function sseRoutes(app: FastifyInstance) {
     };
 
     send("connected", { clientId, connected: true });
-
     clients.set(clientId, { id: clientId, write: send, reply });
 
     request.raw.on("close", () => {
       clients.delete(clientId);
       console.log("Cliente SSE desconectado:", clientId);
     });
-  })
+  });
 }
